b268f91d745cafb0ff97cda8a56a787338b54136
Ketsero <164652237+Ketsero@users.noreply.github.com>
2025-03-24 18:58:17 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,30 +1,3 @@
-let load = document.querySelector('.loading')
-window.addEventListener('DOMContentLoaded', function() {
-    anime({
-        targets: '.loading',
-        keyframes: [
-            {translateX: 160},
-            {translateY: 89},
-            {translateX: 273},
-            {translateY: -90},
-            {translateX: 160},
-            {translateY: 0},
-            {translateX: 350},
-            {translateX: 430, translateY: 90},
-            {translateX: 510},
-            {translateY: -90},
-            {translateX: 420, translateY: 0},
-            {translateX: 607},
-        ],
-        duration: 10000,
-        easing: 'linear',
-        complete: () => {
-            window.location.href = "golov2.html";
-        }
-    })
-});
-
-
 function checkPassword() {
     let input = document.querySelector('.input');
     if (input.value == "4shprog") {
